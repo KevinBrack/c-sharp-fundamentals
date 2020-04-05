960e321b199a3382262e0e9468e71027ed648665
Kevin Brack <brackkevin@gmail.com>
2020-04-05 10:54:37 -0500

    init

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+  public class BookTests
+  {
+    [Fact]
+    public void BookCalculatesAnAverageGrade()
+    {
+
+      // arrange
+      var book = new Book("");
+      book.AddGrade(89.1);
+      book.AddGrade(90.1);
+      book.AddGrade(77.3);
+
+      // act
+      var result = book.GetStatistics();
+
+      // assert
+      Assert.Equal(85.5, result.Average, 1);
+      Assert.Equal(90.1, result.High, 1);
+      Assert.Equal(77.3, result.Low, 1);
+    }
+  }
+}
