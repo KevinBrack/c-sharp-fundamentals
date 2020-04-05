852a411ecd5391de9086fa3da89e95f3a67ee41f
Kevin Brack <brackkevin@gmail.com>
2020-04-05 13:07:58 -0500

    testing book

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -3,25 +3,39 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-  public class BookTests
-  {
-    [Fact]
-    public void BookCalculatesAnAverageGrade()
+    public class BookTests
     {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
 
-      // arrange
-      var book = new Book("");
-      book.AddGrade(89.1);
-      book.AddGrade(90.1);
-      book.AddGrade(77.3);
+            // arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.1);
+            book.AddGrade(77.3);
 
-      // act
-      var result = book.GetStatistics();
+            // act
+            var result = book.GetStatistics();
 
-      // assert
-      Assert.Equal(85.5, result.Average, 1);
-      Assert.Equal(90.1, result.High, 1);
-      Assert.Equal(77.3, result.Low, 1);
+            // assert
+            Assert.Equal(85.5, result.Average, 1);
+            Assert.Equal(90.1, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+        }
+
+        [Fact]
+        public void BookDoesNotAcceptInvalidValues()
+        {
+            var book = new Book("Valid");
+
+            book.AddGrade(80);
+            book.AddGrade(105);
+            book.AddGrade(-3);
+
+            var result = book.GetStatistics();
+
+            Assert.Equal(80, result.Average);
+        }
     }
-  }
 }
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -3,128 +3,143 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-  public class TypeTests
-  {
-    [Fact]
-    public void ValueTypesAlsoPassByValue()
+    public class TypeTests
     {
-      var x = GetInt();
-      var y = GetInt();
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Kevin";
+            var upper = MakeUppercase(name);
 
-      SetIntRef(ref x);
-      SetInt(y);
+            Assert.Equal("Kevin", name);
+            Assert.Equal("KEVIN", upper);
+        }
 
-      Assert.Equal(42, x);
-      Assert.Equal(3, y);
+        private string MakeUppercase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+            var x = GetInt();
+            var y = GetInt();
+
+            SetIntRef(ref x);
+            SetInt(y);
+
+            Assert.Equal(42, x);
+            Assert.Equal(3, y);
+        }
+
+        private void SetInt(int y)
+        {
+            y = 42;
+        }
+
+        private void SetIntRef(ref int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CSharpCanPassByOutParam()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByOut(out book1, "New Name");
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetNameByOut(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpCanPassByReference()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetNameByRef(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void GetBookSetNameByRef(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
+        public void GetBookReturnsDifferentObjects()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+
+            // act
+
+            // assert
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotEqual(book1.Name, book2.Name);
+            Assert.NotSame(book1, book2);
+            Assert.False(Object.ReferenceEquals(book1, book2));
+        }
+
+        [Fact]
+        public void TwoVariablesCanReferenceSameObject()
+        {
+            // arrange
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            // act
+
+            // assert
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 1", book2.Name);
+            Assert.Equal(book1.Name, book2.Name);
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
+        }
     }
-
-    private void SetInt(int y)
-    {
-      y = 42;
-    }
-
-    private void SetIntRef(ref int x)
-    {
-      x = 42;
-    }
-
-    private int GetInt()
-    {
-      return 3;
-    }
-
-    [Fact]
-    public void CSharpCanPassByOutParam()
-    {
-      var book1 = GetBook("Book 1");
-      GetBookSetNameByOut(out book1, "New Name");
-      Assert.Equal("New Name", book1.Name);
-    }
-
-    private void GetBookSetNameByOut(out Book book, string name)
-    {
-      book = new Book(name);
-    }
-
-    [Fact]
-    public void CSharpCanPassByReference()
-    {
-      var book1 = GetBook("Book 1");
-      GetBookSetNameByRef(ref book1, "New Name");
-
-      Assert.Equal("New Name", book1.Name);
-    }
-
-    private void GetBookSetNameByRef(ref Book book, string name)
-    {
-      book = new Book(name);
-    }
-
-    [Fact]
-    public void CSharpIsPassByValue()
-    {
-      var book1 = GetBook("Book 1");
-      GetBookSetName(book1, "New Name");
-
-      Assert.Equal("Book 1", book1.Name);
-    }
-
-    private void GetBookSetName(Book book, string name)
-    {
-      book = new Book(name);
-    }
-
-    [Fact]
-    public void CanSetNameFromReference()
-    {
-      var book1 = GetBook("Book 1");
-      SetName(book1, "New Name");
-
-      Assert.Equal("New Name", book1.Name);
-    }
-
-    private void SetName(Book book, string name)
-    {
-      book.Name = name;
-    }
-
-    [Fact]
-    public void GetBookReturnsDifferentObjects()
-    {
-      // arrange
-      var book1 = GetBook("Book 1");
-      var book2 = GetBook("Book 2");
-
-      // act
-
-      // assert
-      Assert.Equal("Book 1", book1.Name);
-      Assert.Equal("Book 2", book2.Name);
-      Assert.NotEqual(book1.Name, book2.Name);
-      Assert.NotSame(book1, book2);
-      Assert.False(Object.ReferenceEquals(book1, book2));
-    }
-
-    [Fact]
-    public void TwoVariablesCanReferenceSameObject()
-    {
-      // arrange
-      var book1 = GetBook("Book 1");
-      var book2 = book1;
-
-      // act
-
-      // assert
-      Assert.Equal("Book 1", book1.Name);
-      Assert.Equal("Book 1", book2.Name);
-      Assert.Equal(book1.Name, book2.Name);
-      Assert.Same(book1, book2);
-      Assert.True(Object.ReferenceEquals(book1, book2));
-    }
-
-    Book GetBook(string name)
-    {
-      return new Book(name);
-    }
-  }
 }
